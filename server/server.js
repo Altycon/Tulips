01bedacda7e44d4cb5a19038ce5772c6beed9d7d
Altycon <altycon1@gmail.com>
2023-05-18 15:01:36 -0700

    trial and error1

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ import path from 'path';
 
 import { ROUTES } from './routes/routes.js';
 import { indexRouter } from './routes/index_router.js';
-import { authRouter } from './routes/auth_router.js';
-import { userRouter } from './routes/user_router.js';
+//import { authRouter } from './routes/auth_router.js';
+//import { userRouter } from './routes/user_router.js';
 
 env.config();
 
@@ -20,7 +20,7 @@ App.use(express.static('client'));
 App.use('/favicon.ico', express.static('img/icons/favicon.ico'));
 
 App.use(ROUTES.GET.INDEX, indexRouter);
-App.use(ROUTES.GET.AUTH, authRouter);
-App.use(ROUTES.GET.USER, userRouter);
+//App.use(ROUTES.GET.AUTH, authRouter);
+//App.use(ROUTES.GET.USER, userRouter);
 
 App.listen(port, ()=> console.log(`Tulips server listening on port ${port}`));
